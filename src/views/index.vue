42e7e2b3f20cb5b94e5a7eca47e9ac1e605764ec
zhurainyk <zhurainyk@163.com>
2020-12-23 21:04:18 +0800

    feat: test

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div class="list">1</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "index",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
